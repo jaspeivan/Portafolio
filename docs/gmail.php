ee54d153d1662d2b45d0683563703357c0853941
jaspeivan <Jaspeivan1@gmail.com>
2021-02-07 23:40:01 -0400

    Modificando Index

--- docs/gmail.php
+++ docs/gmail.php
@@ -35,25 +35,14 @@ try {
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'jaspeivan1@gmail.com';                     // SMTP username
     $mail->Password   = 'ivan25213024';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = 0;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     $mail->SMTPSecure = 'tls';
 
     //Recipients
     $mail->setFrom('jaspeivan1@gmail.com',"Portafolio");
     $mail->addAddress('jaspeivan1@gmail.com');     // Add a recipient
-    //$mail->addAddress('ellen@example.com');               // Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
 
-    // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-    // Content
-    // 
-    
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Contacto de Portafolio';
     $mail->Body    =  $body;
@@ -71,10 +60,8 @@ try {
 }
 
 
-
-
-
 /*
+
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
@@ -91,13 +78,25 @@ $to = "jaspeivan1@gmail.com"; // Add your email address in between the "" replac
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: jaspeivan1@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$header = "Reply-To: $email";	
 
-if(mail($to, $subject, $body, $header))
-$mail->send();
-  http_response_code(500);
+if(mail($to, $subject, $body, $header)){
+
+    echo $name;
+
+}else{
+
+    http_response_code(500);
+}
+  
+ 
+
 
 
 */
+
+
+
+
 
 ?>
